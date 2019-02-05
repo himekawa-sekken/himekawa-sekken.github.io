d269b43a3e73188fa851e56874c635f591c28138
tamaina <tamaina@hotmail.co.jp>
2019-02-05 10:33:04 +0900

    wip

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-/* workbox 2019-02-05T00:38:32.641Z */
+/* workbox 2019-02-05T01:32:28.394Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.routing.registerRoute(
@@ -10,7 +10,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute([
     {
         url: '/offline/',
-        revision: '1549327112641',
+        revision: '1549330348394',
     }
 ]);
 workbox.skipWaiting();
